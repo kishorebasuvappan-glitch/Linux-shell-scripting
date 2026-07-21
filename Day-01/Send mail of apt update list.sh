09cf609f44596495722ec58d2dbc431722b2d460
Kishore B <kbasuvappan@gmail.com>
2026-07-21 14:14:08 +0530

    Send mail of apt update list.sh

---
+++ Day-01/Send mail of apt update list.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+DATE=$(date +"%d/%m/%Y")
+START_TIME="05:00 AM"
+END_TIME="09:00 AM"
+DURATION="4 hours"
+
+CSV_FILE="/tmp/apt_update_$(date +%Y%m%d).csv"
+
+# Create CSV
+echo "Status,Repository,Details" > "$CSV_FILE"
+
+apt update 2>&1 | awk '{print $1","$2","$3}' >> "$CSV_FILE"
+
+
+# Email body with dynamic date
+BODY="
+This is advance notification that a maintenance window has been scheduled for the Unix/Linux Production environment.
+
+The full list of systems in scope is in the attached spreadsheet. Additionally, there is an excel for each of the VMs containing the list of patches that are expected to be applied.
+
+Maintenance window
+
+Date: $DATE
+Start: $START_TIME
+End: $END_TIME
+Estimated duration: $DURATION
+
+Please expect a brief outage during the maintenance window as it is a full patching session and the systems will be rebooted after the patches are applied.
+
+We plan to perform the maintenance outside business hours to reduce the outage caused.
+
+If you have any concerns, or need to reschedule, please contact the team.
+"
+
+
+echo "$BODY" | mailx \
+-s "Unix/Linux Production Maintenance Notification - $DATE" \
+-a "$CSV_FILE" \
+team@example.com
